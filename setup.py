bc17d67ba6dfacb1899a73ad95709fb3ad68139d
duyongan <13261051171@163.com>
2022-11-25 11:03:28 +0800

    调整readme

--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
 from setuptools import setup, find_packages
 
-__version__ = "0.0.1.dev"
+__version__ = "0.0.1.dev.5"
 
 setup(
     name='sunstreaker',
     version=__version__,
     packages=find_packages(exclude=["examples"]),
-    python_requires="==3.7",
+    python_requires=">=3.10",
     install_requires=[
         "matplotlib==3.6.2",
         "msgpack==1.0.4",
         "networkx==2.8.8",
         "pandas==1.5.1",
-        "tensorflow==2.10.0",
-        "tensorflow_datasets==4.7.0",
         "dill==0.3.6",
         "tqdm==4.64.1",
         "numpy==1.23.4",
         "msgpack==1.0.4"
     ],
-    url='https://github.com/umangjpatel/kerax',
+    url='https://github.com/duyongan/sunstreaker',
     license='',
     author='duyongan (杜永安)【公众号：无数据不智能】',
     author_email='13261051171@163.com',
